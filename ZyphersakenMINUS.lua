46892f894608affae7ba24475a1b430f80b9f115
UnlessDahell <148602200+UnlessDahell@users.noreply.github.com>
2025-03-18 10:40:26 +0700

    ZyphersakenMINUS.lua

--- ZyphersakenMINUS.lua
+++ ZyphersakenMINUS.lua
@@ -1,1 +1,49 @@
-local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+if not game:IsLoaded() then
+   game.Loaded:Wait() 
+end
+
+local success, Rayfield = pcall(function()
+   return loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+end)
+
+if not success then
+   warn("Failed to load Rayfield UI. Please check the URL or try again later.")
+   return
+end
+
+local Window = Rayfield:CreateWindow({
+   Name = "Zypher Hub (.gg/wDj3ve2K)",
+   Icon = 82284779245358, 
+   LoadingTitle = "Wait until GUI setup",
+   LoadingSubtitle = "by Sir.Zypher and Skider",
+   Theme = "nil",
+
+   DisableRayfieldPrompts = false,
+   DisableBuildWarnings = false,
+
+   ConfigurationSaving = {
+      Enabled = false,
+      FolderName = nil, 
+      FileName = "Zypher"
+   },
+
+   Discord = {
+      Enabled = false, 
+      Invite = "https://discord.gg/wDj3ve2K", 
+      RememberJoins = true 
+   },
+
+   KeySystem = false, 
+   KeySettings = {
+      Title = "Zypher Hub",
+      Subtitle = "Loader has no key!",
+      Note = "Join Discord for news!!", 
+      FileName = "ZTeam", 
+      SaveKey = true, 
+      GrabKeyFromSite = false, 
+      Key = {"ZTeam"} 
+   }
+})
+
+local MainTab = Window:CreateTab("Main", 132272873219669)
+local MainSection = MainTab:CreateSection("Main")
