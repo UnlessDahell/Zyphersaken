90ab44d3bb63b8b59cc3b2fd9cc3e086ade303a4
UnlessDahell <148602200+UnlessDahell@users.noreply.github.com>
2025-03-18 16:51:23 +0700

    ZyphersakenMINUS.lua

--- ZyphersakenMINUS.lua
+++ ZyphersakenMINUS.lua
@@ -60,3 +60,12 @@ local Slider = MainTab:CreateSlider({
         game.Workspace.CurrentCamera.FieldOfView = clampedValue
     end
 })
+
+local CommunitySection = MainTab:CreateSection("Our Discord Community Server")
+
+local Button2 = MainTab:CreateButton({
+   Name = "Discord Link Click to Get Here!",
+   Callback = function()
+      setclipboard("https://discord.gg/wDj3ve2K")
+   end,
+})
